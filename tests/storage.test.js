37920507c236da629ceb2ff0850545e50a88ba14
ZawadiSumba66 <rosesumba98@gmail.com>
2021-04-30 12:14:45 +0300

    Add tests for the storage.js

--- tests/storage.test.js
+++ tests/storage.test.js
@@ -2,7 +2,7 @@ import * as projects from '../src/project';
 import storage from '../src/storage';
 
 describe('saveLocal', () => {
-    it('save the projects array', () => {
+    it('saves projects in the local storage', () => {
       storage.saveLocal();
       expect(JSON.parse(localStorage.projects)).toEqual(projects.projectModule.getProjectsArray());
     });
